c0f180ab5d406ec72b64275a5312e4ace4950c5d
US\amlall <amlall@deloitte.com>
2023-08-20 16:08:52 +0530

    static url fixes

--- PyChat/urls.py
+++ PyChat/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from registration import views as rv
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("chat.urls")),
     path('signup/', rv.SignUp, name="register"),
     path("", include("django.contrib.auth.urls")),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
